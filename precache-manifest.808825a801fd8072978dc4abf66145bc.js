99d6ecde084cb9ac387e2a017ec71e33d15f2029
Antony Solis <antonysolis@live.ca>
2020-01-30 19:15:45 -0600

    Updates

---
+++ precache-manifest.808825a801fd8072978dc4abf66145bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "282e64554a2647785e8611bde26b9b29",
+    "url": "/newsfeed/index.html"
+  },
+  {
+    "revision": "6aef9cdc512531c78709",
+    "url": "/newsfeed/static/css/main.d090c75e.chunk.css"
+  },
+  {
+    "revision": "41848eff3a1278d9d9a9",
+    "url": "/newsfeed/static/js/2.8c031f00.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/newsfeed/static/js/2.8c031f00.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6aef9cdc512531c78709",
+    "url": "/newsfeed/static/js/main.6d9dda76.chunk.js"
+  },
+  {
+    "revision": "9ec438dd3736056dc5c5",
+    "url": "/newsfeed/static/js/runtime-main.ce13ed92.js"
+  }
+]);
